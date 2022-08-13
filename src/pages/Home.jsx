0c02c45b3c7a34f82e36cfd19ee64f8b07bde39a
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-08-12 17:45:00 -0700

    random movie is shown

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Main from '../comps/Main'
+import Row from '../comps/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
-    <Main />
+    <>
+        <Main />
+        <Row title='Up Coming' fetchURL={requests.requestUpcoming} />
+        <Row title='Popular' fetchURL={requests.requestPopular} />
+        <Row title='Trending' fetchURL={requests.requestTrending} />
+        <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+        <Row title='Horror' fetchURL={requests.requestHorror} />
+    </>
   )
 }
 
