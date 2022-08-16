35f2bf1eccd6fbb752f82500498220dd53f366b2
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-08-16 13:48:28 -0700

    connecting to firebase

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-### This is Netflix clome
+### This is Netflix clone
--- src/firebase.js
+++ src/firebase.js
@@ -17,7 +17,5 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-export default app;
